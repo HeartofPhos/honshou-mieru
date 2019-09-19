a3da5659de44011cfec98d1fa4c5708177f92f5e
Heart <>
2019-09-19 12:32:24 +0200

    Updates

---
+++ precache-manifest.1be336e732338d78fe1c808e575598cd.js
@@ -0,0 +1,52 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "24212bfda72dda061b9062c6cd254585.svg"
+  },
+  {
+    "url": "316c8904dd12145e192496b3574110a1.svg"
+  },
+  {
+    "revision": "b2a9f2ad00a4089bce5d4bac9a0cf030",
+    "url": "752e38d0584b06a13f94.worker.js"
+  },
+  {
+    "revision": "47a01cb10f80097bb7385a960ebf1b19",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "819eb6c308f4aa6400becc7f7bec3720",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "453bcef8fac950424bcb40a08cea8687",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "aeb36d21d94d07fa4e3e74f23f21c74d",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "277195f2b2e5d8ebfa4930bb503ed634",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "085c32eb6143751563885d4ee0b8616c",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "23b5460dcd0eab5b93a524cdbab6a9d7",
+    "url": "index.html"
+  },
+  {
+    "revision": "97fe88087d6fb974edd7",
+    "url": "main.bundle.js"
+  },
+  {
+    "revision": "97fe88087d6fb974edd7",
+    "url": "main.css"
+  },
+  {
+    "revision": "843deedd32e8680c2f9e2d51b5474cfb",
+    "url": "site.webmanifest"
+  }
+]);
